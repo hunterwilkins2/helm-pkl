afed7ac232447c01671fc0f2a50af19406b87538
Hunter Wilkins <hunter.wilkins2@gmail.com>
2025-08-02 10:27:56 -0400

    implemented helm-pkl.sh

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Helm Pkl Plugin
+
+Wrapper around helm to use [pkl](https://pkl-lang.org/main/current/index.html) instead of yaml for values
+
+```sh
+helm pkl [command] [--pkl-template TEMPLATE] [flags]
+```
+
+## Requirements
+
+* pkl 0.28.2
+
+## Install
+
+```sh
+helm plugin install https://github.com/hunterwilkins2/helm-pkl
+```
+
+## Usage
+
+```sh
+Usage:
+  helm pkl [command] [--pkl-template TEMPLATE] [flags]
+
+Available commands:
+  install     install a chart
+  lint        examine a chart for possible issues
+  template    locally render templates
+  upgrade     upgrade a release
+
+Flags:
+      --values-file string		save compliled YAML output from pkl to file
+      --burst-limit int                 client-side default throttling limit (default 100)
+      --debug                           enable verbose output
+  -h, --help                            help for helm
+      --kube-apiserver string           the address and the port for the Kubernetes API server
+      --kube-as-group stringArray       group to impersonate for the operation, this flag can be repeated to specify multiple groups.
+      --kube-as-user string             username to impersonate for the operation
+      --kube-ca-file string             the certificate authority file for the Kubernetes API server connection
+      --kube-context string             name of the kubeconfig context to use
+      --kube-insecure-skip-tls-verify   if true, the Kubernetes API server's certificate will not be checked for validity. This will make your HTTPS connections insecure
+      --kube-tls-server-name string     server name to use for Kubernetes API server certificate validation. If it is not provided, the hostname used to contact the server is used
+      --kube-token string               bearer token used for authentication
+      --kubeconfig string               path to the kubeconfig file
+  -n, --namespace string                namespace scope for this request (default "default")
+      --qps float32                     queries per second used when communicating with the Kubernetes API, not including bursting
+      --registry-config string          path to the registry config file 
+      --repository-cache string         path to the directory containing cached repository indexes 
+      --repository-config string        path to the file containing repository names and URLs 
+```a
+
---
+++ helm-pkl.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+function usage_missing_command() {
+  printf 'Usage:\n'
+  printf '  helm pkl [command] [--pkl-template TEMPLATE] [flags]\n\n'
+  printf 'Available commands:\n'
+  helm --help | 
+    grep -e '^\s\sinstall' -e '^\s\supgrade' -e '^\s\stemplate' -e '^\s\slint'
+  printf '\n'
+  helm --help | 
+    sed -n '/^Flags:$/,$p' |
+    awk 'NR==2{print "      --values-file string\t\tsave compliled YAML output from pkl to file"}1' |
+    head -n -1
+  exit 1
+}
+
+function usage_command() {
+  printf 'Usage:\n'
+  printf '  helm pkl %s [--pkl-template TEMPLATE] [NAME] [CHART] [flags]\n\n' "$1"
+  helm "$1" --help | 
+    sed -n '/^Flags:$/,$p' |
+    awk 'NR==2{print "      --values-file string\t\t\t   save compliled YAML output from pkl to file"}1'
+  printf "\n"
+  exit 1
+}
+
+case "$1" in
+  install|upgrade|template|lint) helm_command="$1"; shift;;
+  *) usage_missing_command;;
+esac
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --pkl-template) pkl_file="$2"; shift; shift;;
+    --values-file) values_file="$2"; shift; shift;;
+    --help|-h) usage_command "$helm_command";;
+    *) helm_args+="$1 "; shift;;
+  esac
+done
+
+for helm_env in $(printenv | grep 'HELM'); do
+  IFS="=" read -r helm_var helm_value <<< "$helm_env"
+  if [ "$helm_value" ]; then
+    case "$helm_var" in
+      HELM_BURST_LIMIT) helm_args+='--burst-limit "$helm_value" ';;
+      HELM_DEBUG) helm_args+='--debug ';;
+      HELM_KUBEAPISERVER) helm_args+='--kube-apiserver "$helm_value" ';;
+      HELM_KUBEASGROUPS) helm_args+='--kube-as-group "$helm_value" ';;
+      HELM_KUBEASUSER) helm_args+='--kube-as-user "$helm_value" ';;
+      HELM_KUBECAFILE) helm_args+='--kube-ca-file "$helm_value" ';;
+      HELM_KUBECONTEXT) helm_args+='--kube-context "$helm_value" ';;
+      HELM_KUBEINSECURE_SKIP_TLS_VERIFY) helm_args+='--kube-insecure-skip-tls-verify "$helm_value" ';;
+      HELM_KUBETLS_SERVER_NAME) helm_args+='--kube-tls-server-name "$helm_value" ';;
+      HELM_KUBETOKEN) helm_args+='--kube-token "$helm_value" ';;
+      HELM_KUBECONFIG) helm_args+='--kubeconfig "$helm_value" ';;
+      HELM_NAMESPACE) helm_args+='--namespace "$helm_value" ';;
+      HELM_QPS) helm_args+='--qps "$helm_value"';;
+      HELM_REGISTRY_CONFIG) helm_args+='--registry-config "$helm_value" ';;
+      HELM_REPOSITORY_CACHE) helm_args+='--repository-cache "$helm_value" ';;
+      HELM_REPOSITORY_CONFIG) helm_args+='--repository-config "$helm_value" ';;
+    esac
+  fi
+done
+
+if [[ -z "$pkl_file" ]]; then
+  usage_command "$helm_command"
+fi
+
+if [[ -z "$values_file" ]]; then
+  values_file=$(mktemp)
+  trap "rm -f $values_file" 0 2 3 15
+fi
+
+pkl eval \
+  --format=yaml \
+  --output-path="$values_file" \
+  "$pkl_file"
+
+helm "$helm_command" \
+  --values "$values_file" \
+  "$helm_args"
+
