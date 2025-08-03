c48987970b0f47da5d7db42dff9bbae8f23cfb3e
Hunter Wilkins <hunter.wilkins2@gmail.com>
2025-08-03 17:09:59 -0400

    exit early if pkl validation fails

--- helm-pkl.sh
+++ helm-pkl.sh
@@ -40,24 +40,24 @@ done
 
 for helm_env in $(printenv | grep 'HELM'); do
   IFS="=" read -r helm_var helm_value <<< "$helm_env"
-  if [ "$helm_value" ]; then
+  if [[ "$helm_value" != "false" && "$helm_value" != "" ]]; then
     case "$helm_var" in
-      HELM_BURST_LIMIT) helm_args+='--burst-limit "$helm_value" ';;
-      HELM_DEBUG) helm_args+='--debug ';;
-      HELM_KUBEAPISERVER) helm_args+='--kube-apiserver "$helm_value" ';;
-      HELM_KUBEASGROUPS) helm_args+='--kube-as-group "$helm_value" ';;
-      HELM_KUBEASUSER) helm_args+='--kube-as-user "$helm_value" ';;
-      HELM_KUBECAFILE) helm_args+='--kube-ca-file "$helm_value" ';;
-      HELM_KUBECONTEXT) helm_args+='--kube-context "$helm_value" ';;
-      HELM_KUBEINSECURE_SKIP_TLS_VERIFY) helm_args+='--kube-insecure-skip-tls-verify "$helm_value" ';;
-      HELM_KUBETLS_SERVER_NAME) helm_args+='--kube-tls-server-name "$helm_value" ';;
-      HELM_KUBETOKEN) helm_args+='--kube-token "$helm_value" ';;
-      HELM_KUBECONFIG) helm_args+='--kubeconfig "$helm_value" ';;
-      HELM_NAMESPACE) helm_args+='--namespace "$helm_value" ';;
-      HELM_QPS) helm_args+='--qps "$helm_value"';;
-      HELM_REGISTRY_CONFIG) helm_args+='--registry-config "$helm_value" ';;
-      HELM_REPOSITORY_CACHE) helm_args+='--repository-cache "$helm_value" ';;
-      HELM_REPOSITORY_CONFIG) helm_args+='--repository-config "$helm_value" ';;
+      HELM_BURST_LIMIT) helm_args+="--burst-limit $helm_value ";;
+      HELM_DEBUG) helm_args+="--debug ";;
+      HELM_KUBEAPISERVER) helm_args+="--kube-apiserver $helm_value ";;
+      HELM_KUBEASGROUPS) helm_args+="--kube-as-group $helm_value ";;
+      HELM_KUBEASUSER) helm_args+="--kube-as-user $helm_value ";;
+      HELM_KUBECAFILE) helm_args+="--kube-ca-file $helm_value ";;
+      HELM_KUBECONTEXT) helm_args+="--kube-context $helm_value ";;
+      HELM_KUBEINSECURE_SKIP_TLS_VERIFY) helm_args+="--kube-insecure-skip-tls-verify ";;
+      HELM_KUBETLS_SERVER_NAME) helm_args+="--kube-tls-server-name $helm_value ";;
+      HELM_KUBETOKEN) helm_args+="--kube-token $helm_value ";;
+      HELM_KUBECONFIG) helm_args+="--kubeconfig $helm_value ";;
+      HELM_NAMESPACE) helm_args+="--namespace $helm_value ";;
+      HELM_QPS) helm_args+="--qps $helm_value ";;
+      HELM_REGISTRY_CONFIG) helm_args+="--registry-config $helm_value ";;
+      HELM_REPOSITORY_CACHE) helm_args+="--repository-cache $helm_value ";;
+      HELM_REPOSITORY_CONFIG) helm_args+="--repository-config $helm_value ";;
     esac
   fi
 done
@@ -74,9 +74,9 @@ fi
 pkl eval \
   --format=yaml \
   --output-path="$values_file" \
-  "$pkl_file"
+  "$pkl_file" || exit 1
 
 helm "$helm_command" \
-  --values "$values_file" \
-  "$helm_args"
+  $helm_args \
+  --values "$values_file" 
 
